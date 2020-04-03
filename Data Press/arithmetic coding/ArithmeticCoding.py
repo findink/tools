5b26cb448e2de6d6acd92cc41a210c65ac09fc01
findink <lgq7000@163.com>
2020-04-03 22:05:09 +0800

    解码时有问题.带换思路.

---
+++ Data Press/arithmetic coding/ArithmeticCoding.py
@@ -0,0 +1,85 @@
+#Developed By
+#NISHAN POOJARY
+
+
+from collections import Counter
+import numpy as np
+
+print("Enter a Sequence\n")
+inputstr = input()
+print  (inputstr + "\n")
+
+res = Counter(inputstr)
+print (str(res))
+
+#sortlist = sorted(res.iteritems(), lambda x, y : cmp(x[1], y[1]), reverse = True)
+#print sortlist
+M = len(res)
+#print (M)
+N = 5
+A = np.zeros((M,5),dtype=object)
+
+#A = [[0 for i in range(N)] for j in range(M)]
+
+reskeys = list(res.keys())
+resvalue = list(res.values())
+totalsum = sum(resvalue) # totalsum = len(inputstr)
+
+# Creating Table
+
+A[M-1][3] = 0
+for i in range(M):
+   A[i][0] = reskeys[i]
+   A[i][1] = resvalue[i]
+   A[i][2] = ((resvalue[i]*1.0)/totalsum)
+i=0
+A[M-1][4] = A[M-1][2]
+while i < M-1:
+   A[M-i-2][4] = A[M-i-1][4] + A[M-i-2][2]
+   A[M-i-2][3] = A[M-i-1][4]
+   i+=1
+print (A)
+
+# Encoding
+
+print("\n------- ENCODING -------\n" )
+strlist = list(inputstr)
+LEnco = []
+UEnco = []
+LEnco.append(0)
+UEnco.append(1)
+
+for i in range(len(strlist)):
+    result = np.where(A == reskeys[reskeys.index(strlist[i])])
+    addtollist = (LEnco[i] + (UEnco[i] - LEnco[i])*float(A[result[0],3]))
+    addtoulist = (LEnco[i] + (UEnco[i] - LEnco[i])*float(A[result[0],4]))
+
+    LEnco.append(addtollist)
+    UEnco.append(addtoulist)
+
+    tag = (LEnco[-1] + UEnco[-1])/2.0  # 最终返回中间值
+
+LEnco.insert(0, " Lower Range")
+UEnco.insert(0, "Upper Range")
+print(np.transpose(np.array(([LEnco],[UEnco]),dtype=object)))
+print("\nThe Tag is \n ")
+print(tag)
+
+# Decoding
+
+print("\n------- DECODING -------\n" )
+ltag = 0
+utag = 1
+decodedSeq = []
+for i in range(len(inputstr)):
+    numDeco = ((tag - ltag)*1.0)/(utag - ltag)
+    for i in range(M):
+        if (float(A[i,3]) < numDeco < float(A[i,4])):
+
+            decodedSeq.append(str(A[i,0]))
+            ltag = float(A[i,3])
+            utag = float(A[i,4])
+            tag = numDeco
+
+print("The decoded Sequence is \n ")
+print("".join(decodedSeq))
--- Data Press/arithmetic coding/arithmetic_coding.py
+++ Data Press/arithmetic coding/arithmetic_coding.py
@@ -1,54 +1,90 @@
 import sys
 import math
+from collections import Counter
 
-def encode(filePath):
-    origin = open(filePath,'r')
-    origin_str = ''
-    for line in origin: # 将文件合成一个字符串.
-        origin_str += line
-    site_dict = getSiteDict(origin_str)
-    low,high = 0,1
-    f = open(sys.path[0]+"/encoded.txt",'w')
-    for c in origin_str:
-        low_ = low +  (high-low)*site_dict[c][0]
-        high_ = low + (high-low)*site_dict[c][1]
-        low,high = low_,high_
-        if high - low < 1e-3:
-            low *= 100
-            high *= 100
-            store_num = math.floor(low)
-            if store_num != 0:
-                f.write(str(store_num))
-                low -= store_num
-                high -= store_num
-    f.write(str(low).replace('.',''))
-    print(low,high)
-
-    
-
-    
-    
-def getSiteDict(origin_str):
-    char_dict = {}
-    for c in origin_str: # 统计各个字符的出现次数
-        if c in char_dict.keys():
-            char_dict[c] += 1
-        else:
-            char_dict.update({c:1})
+class arithmeticCoding():
     char_ratio_dict = {}
     site_dict = {}
-    origin_str_len = len(origin_str)
-    low = 0
-    for (k,v) in char_dict.items():
-        ratio = v/origin_str_len
-        char_ratio_dict.update({k:ratio})
-        site_dict.update({k:[low,low + ratio]})
-        low += ratio
-    return site_dict
+    origin_str = ''
+    
+    def __init__(self):
+        pass
+    def encode(self,filePath):
+        origin = open(filePath,'r')
+        for line in origin: # 将文件合成一个字符串.
+            self.origin_str += line
+        self.site_dict = self.getSiteDict()
+        low,high = 0,1
+        f = open(sys.path[0]+"/encoded.txt",'w')
+        for c in self.origin_str:
+            low_ = low +  (high-low)*self.site_dict[c][0]
+            high_ = low + (high-low)*self.site_dict[c][1]
+            low,high = low_,high_
+            if high - low < 1e-5: # 移动小数点, 将整数部分存入文件
+                low *= 1e2
+                high *= 1e2
+                store_num = math.floor(low)
+                if store_num < 10:
+                    f.write('0')
+                if store_num == 0:
+                    f.write('0')
+                if store_num != 0:
+                    f.write(str(store_num))
+                    low -= store_num
+                    high -= store_num
+        f.write(str(low).replace('.',''))
 
 
 
-def decode():
-    pass
+    def getSiteDict(self):
+        char_dict = Counter(self.origin_str)
+        origin_str_len = len(self.origin_str)
+        low = 0
+        for (k,v) in char_dict.items():
+            ratio = round(v/origin_str_len,5)
+            self.site_dict.update({k:[low,low + ratio]})
+            low += ratio
+        return self.site_dict
 
-encode(sys.path[0]+"/origin.txt")
+
+    def getCharBySite(self,site):
+        for k,v in self.site_dict.items():
+            if  v[0] < site <v[1]:
+                return k
+
+
+    def decode(self,filePath):
+        f = open(filePath,'r')
+        r = ''
+        decoded_str = '0.'
+        for line in f:
+            decoded_str += line
+        str_len = len(decoded_str)
+        i,j = 5,10
+        k = 1.0
+        num = float(decoded_str[0:15])
+        count = 0
+        while count < 30:
+            count += 1
+            c = self.getCharBySite(num/k)
+            r += c
+            num -= self.site_dict[c][0] * k
+            k *= self.site_dict[c][1] - self.site_dict[c][0]
+            if k < 1e-5:
+                num *= 1e2
+                k *=   1e2
+                d = 15 - len(str(num))
+                if d > 0:
+                    i = j+1
+                    j = min(j+ d ,str_len)
+                    if j == str_len:
+                        break
+                    num_str = str(num) + decoded_str[i:j]
+                    num = float(num_str) 
+        print(r)
+
+
+if __name__ == "__main__":
+    coding = arithmeticCoding()    
+    coding.encode(sys.path[0]+"/origin.txt")
+    coding.decode(sys.path[0]+"/encoded.txt")
